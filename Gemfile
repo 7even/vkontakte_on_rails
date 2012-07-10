77d164ba279adfa758edd6989c06cb391744beaf
Vsevolod Romashov <7@7vn.ru>
2012-07-10 04:30:23 +0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source :rubygems
+
+gem 'rails', '3.2.6'
+
+gem 'sqlite3'
+gem 'jquery-rails'
+gem 'twitter-bootstrap-rails'
+
+gem 'vkontakte_api', '~> 1.0.rc'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier',     '>= 1.0.3'
+end
+
+gem 'capistrano'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VkontakteOnRails::Application.config.secret_token = 'f5d93a9e8c34bdc7825cc9d592fd4983e7b0ea879832dfcf71d6caa77911a020316f3d7d5c1978a85db9e7efe182f6bcaeba901f528a2bac36aacdcb1f7d869d'
