01fa939103c943896497f9cd46d5f4c224c11664
Vsevolod Romashov <7@7vn.ru>
2012-07-22 01:19:07 +0400

    passing state to VkontakteApi.authorization_url and checking it on callback

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,15 @@
+# encoding: utf-8
 class SessionsController < ApplicationController
   def new
-    @vk_url = VkontakteApi.authorization_url(scope: [:friends, :groups, :offline])
+    srand
+    session[:state] ||= Digest::MD5.hexdigest(rand.to_s)
+    
+    @vk_url = VkontakteApi.authorization_url(scope: [:friends, :groups, :offline, :notify], state: session[:state])
   end
   
   def callback
+    redirect_to root_url, alert: 'Ошибка авторизации, попробуйте войти еще раз.' and return if session[:state].present? && session[:state] != params[:state]
+    
     @vk = VkontakteApi.authorize(code: params[:code])
     session[:token] = @vk.token
     session[:vk_id] = @vk.user_id
